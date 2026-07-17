3ab6e6771fda9e4a8f95037172ca54430bee7814
Zaraba2021 <zaraba2021@gmail.com>
2026-07-17 19:27:08 +0100

    Refactor service worker for simplified caching

--- sw.js
+++ sw.js
@@ -1,21 +1,2 @@
-const CACHE_NAME = "gasoil-v1";
-const ASSETS = [
-    "index.html",
-    "manifest.json"
-];
-
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
-    );
-});
-
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then((res) => {
-            return res || fetch(e.request);
-        })
-    );
-});
+self.addEventListener('install',e=>e.waitUntil(caches.open('gasoil-v1').then(c=>c.addAll(['/']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
